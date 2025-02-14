a6268c80e1648781c059b3fd4bfdcfc9fc106997
jamesfisherCOG <James.Fisher2@cognizant.com>
2025-02-14 15:17:46 +0000

    correcting the config files

--- wdio.conf.js
+++ wdio.conf.js
@@ -20,7 +20,7 @@ exports.config = {
     // The path of the spec files will be resolved relative from the directory of
     // of the config file unless it's absolute.
     //
-    specs: [
+    specs: ['./test/specs/**/*.js'
         // ToDo: define location for spec files here
     ],
     // Patterns to exclude.
