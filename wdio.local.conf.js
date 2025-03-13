0640da22eb0fbb6bdd28f1f859ca173ee838276c
jamesfisherCOG <James.Fisher2@cognizant.com>
2025-03-13 10:37:26 +0000

    fixing the allure reporting

--- wdio.local.conf.js
+++ wdio.local.conf.js
@@ -28,7 +28,7 @@ export const config = {
   // then the current working directory is where your `package.json` resides, so `wdio`
   // will be called from there.
   //
-  specs: ['./test/specs/**/*.js'],
+  specs: ['./test/specs/**/happyPath.js'],
   // Patterns to exclude.
   exclude: [],
   // injectGlobals: false,
@@ -146,7 +146,14 @@ export const config = {
   // see also: https://webdriver.io/docs/dot-reporter
 
   reporters: [
-    'spec',
+    [
+      'spec',
+      {
+        addConsoleLogs: true,
+        realtimeReporting: true,
+        color: false
+      }
+    ],
     [
       'allure',
       {
