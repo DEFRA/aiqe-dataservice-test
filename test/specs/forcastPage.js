1c23539ac541d22d6f2c151d9ab77b59fa79f957
jamesfisherCOG <James.Fisher2@cognizant.com>
2025-03-26 12:16:32 +0000

    test

---
+++ test/specs/forcastPage.js
@@ -0,0 +1,15 @@
+import createLogger from 'helpers/logger'
+import proxyFetch from 'helpers/proxy-fetch'
+const optionsJson = { method: 'GET', headers: { 'Content-Type': 'text/json' } }
+
+
+const logger = createLogger()
+async function pollutantSummaryUrl() {
+  const homeUrl = config.get('homeUrl')
+  logger.info(`forecastSummaryUrl: ${homeUrl}`)
+  const response = await proxyFetch(homeUrl, optionsJson).catch(
+    (err) => {
+      logger.info(`err ${JSON.stringify(err.message)}`)
+    }
+  )
+}
