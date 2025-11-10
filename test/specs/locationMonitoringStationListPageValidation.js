aba1ec5611ee88e0d1a649efb0180de3928075b5
jamesfisherCOG <James.Fisher2@cognizant.com>
2025-11-10 20:39:23 +0000

    small corrections

--- test/specs/locationMonitoringStationListPageValidation.js
+++ test/specs/locationMonitoringStationListPageValidation.js
@@ -232,7 +232,7 @@ describe('monitoring station list page tests', () => {
       expect(styles.color).toBe('rgb(29, 112, 184)')
       expect(styles['font-family']).toBe('"GDS Transport", arial, sans-serif')
       expect(styles['text-decoration']).toBe(
-        'underline 1px solid rgb(11, 12, 12)'
+        'underline 1px solid rgb(29, 112, 184)'
       )
       expect(styles['text-decoration-thickness']).toBe('1px')
       // expect(styles['font-weight']).toBe('700') styling bug
