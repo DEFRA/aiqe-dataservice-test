0082b87f77ba35825e37d8ecfe5891e4abd9c03a
jamesfisherCOG <James.Fisher2@cognizant.com>
2025-11-20 08:57:52 +0000

    tablet tests

--- test/specs/tabletHappyPath.js
+++ test/specs/tabletHappyPath.js
@@ -1047,7 +1047,7 @@ describe('tablet happy Path', () => {
     for (const element of getToggleTip) {
       const styles = await common.getStyles(element, getToggleTipProperties)
       expect(styles['background-color']).toBe('rgb(255, 255, 255)')
-      expect(styles.border).toBe('0px')
+      expect(styles.border).toBe('0px none rgb(11, 12, 12)')
       expect(styles.color).toBe('rgb(11, 12, 12)')
       expect(styles.cursor).toBe('help')
       expect(styles.height).toBe('26px')
