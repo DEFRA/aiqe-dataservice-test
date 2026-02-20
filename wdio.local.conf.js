3e557226400b7ad0922a5db0022f652596fcd869
jamesfisherCOG <James.Fisher2@cognizant.com>
2026-02-20 15:28:18 +0000

    search by location blank search error link test

--- wdio.local.conf.js
+++ wdio.local.conf.js
@@ -29,7 +29,7 @@ export const config = {
   // then the current working directory is where your `package.json` resides, so `wdio`
   // will be called from there.
   //
-  specs: ['./test/specs/disableJavascriptTests/noJavascriptValidation.js'],
+  specs: ['./test/specs/regressionSearchByLocation/errorScenariosAndBugs.js'],
   // Patterns to exclude.
   exclude: [],
   // injectGlobals: false,
