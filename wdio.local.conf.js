163db0474da65f2e84dced3cab8bc1d3edd584d3
jamesfisherCOG <James.Fisher2@cognizant.com>
2026-06-09 16:28:37 +0100

    small fix

--- wdio.local.conf.js
+++ wdio.local.conf.js
@@ -30,7 +30,7 @@ export const config = {
   // will be called from there.
   //
   specs: [
-    './test/specs/regressionCreateACustomDataSet/viewDataSourcesValidation.js'
+    './test/specs/regressionSearchByLocation/locationMonitoringStationListPageValidation.js'
   ],
   // Patterns to exclude.
   exclude: [],
